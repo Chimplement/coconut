e40e7ff0a9b712054c39e3603bff11c1b0770f8c
Tycho <tychosteur@gmail.com>
2024-06-05 21:26:40 +0200

    simple hello world bootloader

---
+++ src/bootsector.s
@@ -0,0 +1,25 @@
+bits 16
+org 0x7c00
+boot:
+    mov si, message
+    call putstr
+
+    cli
+    hlt
+
+putstr:
+    mov ah, 0x0e ; write
+.loop:
+    lodsb
+    cmp al,0 ; end of string
+    jz .ret
+
+    int 0x10 ; video services
+    jmp .loop
+.ret:
+    ret
+
+message: db "Hello, world!",0
+
+times 510 - ($-$$) db 0
+dw 0xaa55 ; magic number
