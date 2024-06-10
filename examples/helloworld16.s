43459d420331ab03727abdbe5c26d63d2d4da959
Tycho <tychosteur@gmail.com>
2024-06-10 22:05:05 +0200

    created an example program thats will be bootable by the bootloader

---
+++ examples/helloworld16.s
@@ -0,0 +1,23 @@
+bits 16
+org 0x00f0
+
+magic: dw 0x800f
+mode: db 0x0
+orgin: dw 0x00f0
+entry: dw start
+
+start:
+    mov si, msg
+    mov ah, 0x0e ; write
+.loop:
+    lodsb
+    cmp al, 0x0
+    jz .hlt
+
+    int 0x10 ; video services
+    jmp .loop
+.hlt:
+    cli
+    hlt
+
+msg: db "Hello, World!", 0x0
